58976b3fd413b6abb1fc8e0ed2c614963ba05c43
Emie-Trialer <emiedonmukumo@gmail.com>
2025-03-29 15:41:32 +0100

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,15 @@ module.exports = {
     siteUrl: 'https://emiedonmokumo.vercel.app',
     generateRobotsTxt: true,
     sitemapSize: 5000,
+    outDir: './public',
+    robotsTxtOptions: {
+      policies: [
+        {
+          userAgent: '*',
+          allow: '/',
+        },
+      ],
+    },
+    
   };
   
