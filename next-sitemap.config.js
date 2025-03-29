fd7c5fea3df66af271558caa0abe0585c8cb7114
Emie-Trialer <emiedonmukumo@gmail.com>
2025-03-29 15:09:32 +0100

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://emiedonmokumo.vercel.app', // Replace with your actual domain
-    generateRobotsTxt: true, // Generates robots.txt as well
-    sitemapSize: 7000,
-    changefreq: 'weekly',
-    priority: 0.7,
-};
+    siteUrl: 'https://emiedonmokumo.vercel.app', // Change this to your site's domain
+    generateRobotsTxt: true, // Generates a robots.txt file
+    changefreq: 'weekly', // Default change frequency
+    priority: 0.7, // Default priority
+    sitemapSize: 5000, // Number of entries per sitemap file
+    // exclude: ['/admin', '/dashboard'], // Exclude any private routes
+  };
+  
